3c9ccdc40091ac82ebcc6ca07b5bd3f9a5bdcf92
Kibur <manu2293@hotmail.com>
2015-04-26 13:51:17 +0200

    Changed the search style. Default search engine is DuckDuckGo. Removed google and youtube since you can use !g and !yt in DDG.

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-$.getJSON("config.json", function(data){
+$.getJSON("config.json", function(data) {
     window.cfg = [
         data.style.heading_font,
         data.style.link_font,
@@ -19,35 +19,42 @@ $.getJSON("config.json", function(data){
         data.ext.width,
         data.ext.opacity
     ];
+
     window.cfg_bool = [
         data.bool.borders,
         data.bool.simplesearch,
         data.bool.alwaysopen,
         data.bool.mascot
     ];
+
     $("span").css({
         "fontFamily": cfg[0],
         "fontSize": cfg[2],
         "color": cfg[6]
     });
+
     $("a").css({
         "fontFamily": cfg[1],
         "fontSize": cfg[3],
         "color": cfg[7]
     });
+
     $("body").css({
         "backgroundColor": cfg[4]
     });
+
     $(".sqr").css({
         "backgroundColor": cfg[5],
         "borderTop": "0 solid " + cfg[8],
         "borderBottom": "0 solid " + cfg[8]
     });
+
     $("#searchinput").css({
         "color": cfg[10],
         "backgroundColor": cfg[11]
     });
-    if(cfg_bool[3]){
+
+    if(cfg_bool[3]) {
         $("#bgimg").css({
             "backgroundImage": "url('" +  cfg[12] + "')",
             "bottom": cfg[13],
@@ -56,71 +63,69 @@ $.getJSON("config.json", function(data){
             "width": cfg[16],
             "opacity": cfg[17]
         });
-    }else{
+    }
+    else {
         $("#bgimg").css({
             "backgroundImage": ""
         });
     }
 });
 
-function evenContainerHeight(){
-	if(window.innerHeight % 2 ==0){
+function evenContainerHeight() {
+	if(window.innerHeight % 2 == 0) {
 		document.getElementById("container").style.height = window.innerHeight;
-	}else{
+	}
+    else {
 		document.getElementById("container").style.height = window.innerHeight - 1;
 	}
 }
 
-window.onresize = function(){
+window.onresize = function() {
 	evenContainerHeight();
 };
 
-window.onload = function(){
+window.onload = function() {
 	evenContainerHeight();
-    document.getElementById("searchinput").addEventListener("keypress", function search(a){
+    document.getElementById("searchinput").addEventListener("keypress", function search(a) {
         var key = a.keyCode;
-        if(key == 13){
+
+        if(key == 13) {
             var query = this.value;
-            if(!cfg_bool[1]){
-                switch(query.substr(0,3)){
-                    case "-g ":
-                        query = query.substr(3);
-                        window.location="https://www.google.com/#q=" + query.replaceChars(" ", "+");
-                        break;
-                    case "-a ":
-                        query = query.substr(3);
-                        window.location="https://duckduckgo.com/?q=" + query.replaceChars(" ", "+");
+
+            if(!cfg_bool[1]) {
+                switch(query.substr(0,3)) {
+                    default:
+                        window.location = "https://duckduckgo.com/?q=" + query.replaceChars(" ", "+");
                         break;
                     case "-d ":
                         query = query.substr(3);
-                        window.location="http://www.donmai.us/posts?tags=" + query.replaceChars(" ", "+");
-                        break;
-                    case "-y ":
-                        query = query.substr(3);
-                        window.location="https://www.youtube.com/results?search_query=" + query.replaceChars(" ", "+");
+                        window.location = "http://www.donmai.us/posts?tags=" + query.replaceChars(" ", "+");
                         break;
                     case "-n ":
                         query = query.substr(3);
-                        window.location="http://www.nicovideo.jp/search/" + query.replaceChars(" ", "%20");
+                        window.location = "http://www.nicovideo.jp/search/" + query.replaceChars(" ", "%20");
                         break;
                     case "-p ":
                         query = query.substr(3);
-                        window.location="http://www.pixiv.net/search.php?s_mode=s_tag&word=" + query.replaceChars(" ", "%20");
+                        window.location = "http://www.pixiv.net/search.php?s_mode=s_tag&word=" + query.replaceChars(" ", "%20");
                         break;
                 }
-            }else{
-                window.location="https://www.google.com/#q=" + query.replaceChars(" ", "+");
+            }
+            else {
+                window.location = "https://www.google.com/#q=" + query.replaceChars(" ", "+");
             }
         }
     });
     
-    document.addEventListener("keypress", function search(a){
+    document.addEventListener("keypress", function search(a) {
         var key = a.keyCode;
-        if(key == 9){
+
+        if(key == 9) {
             var search_sqr = document.getElementById("search_sqr");
-            search_sqr.style.height=300+37+"px";
-            search_sqr.style.borderTop= cfg[9] + " solid " + cfg[8];
-            search_sqr.style.borderBottom= cfg[9] + " solid " + cfg[8];
+            search_sqr.style.height = (300 + 37) + "px";
+            search_sqr.style.borderTop = cfg[9] + " solid " + cfg[8];
+            search_sqr.style.borderBottom = cfg[9] + " solid " + cfg[8];
+
             document.getElementById("searchinput").focus();
         }
     
@@ -133,48 +138,54 @@ window.onload = function(){
     var i = 0;
     var lenSqr = sqr.length;
 
-    if(!cfg_bool[2]){
+    if(!cfg_bool[2]) {
         for(i = 0; i < lenSqr; ++i) {
             sqr[i].addEventListener("mouseover", expand, false);
             sqr[i].addEventListener("mouseout", contract, false);
         }
-    }else{
-        for(i = 0; i < lenSqr; ++i){
+    }
+    else {
+        for(i = 0; i < lenSqr; ++i) {
             var a = 0;
-            for(var x = 0; x < lenSqr; ++x){
-                if(a<sqr[x].getElementsByTagName("a").length){
+
+            for(var x = 0; x < lenSqr; ++x) {
+                if(a<sqr[x].getElementsByTagName("a").length) {
                     a = sqr[x].getElementsByTagName("a").length;
                 }
             }
-            sqr[i].style.height=225+25*a+"px";
-            if(cfg_bool[0]){
-                sqr[i].style.borderTop= cfg[9] + " solid " + cfg[8];
-                sqr[i].style.borderBottom= cfg[9] + " solid " + cfg[8];
+            sqr[i].style.height = (225 + 25 * a) + "px";
+
+            if(cfg_bool[0]) {
+                sqr[i].style.borderTop = cfg[9] + " solid " + cfg[8];
+                sqr[i].style.borderBottom = cfg[9] + " solid " + cfg[8];
             }
         }
     }
 };
 
-function expand(){
+function expand() {
 	var acount = this.getElementsByTagName("a").length;
     var icount = this.getElementsByTagName("input").length;
-    if(icount >= 1){
-        this.style.height=300+37*icount+"px";
-    }else{
-        this.style.height=300+25*acount+"px";
+
+    if(icount >= 1) {
+        this.style.height = (300 + 37 * icount) + "px";
     }
-    if(cfg_bool[0]){
-        this.style.borderTop= cfg[9] + " solid " + cfg[8];
-        this.style.borderBottom= cfg[9] + " solid " + cfg[8];
+    else {
+        this.style.height = (300 + 25 * acount) + "px";
+    }
+
+    if(cfg_bool[0]) {
+        this.style.borderTop = cfg[9] + " solid " + cfg[8];
+        this.style.borderBottom = cfg[9] + " solid " + cfg[8];
     }
 }
-function contract(){
-	this.style.height="150px";
-	this.style.borderTop="0 solid" + cfg[8];
-	this.style.borderBottom="0 solid" + cfg[8];
+function contract() {
+	this.style.height = "150px";
+	this.style.borderTop = "0 solid" + cfg[8];
+	this.style.borderBottom = "0 solid" + cfg[8];
 }
 
-String.prototype.replaceChars = function(character, replacement){
+String.prototype.replaceChars = function(character, replacement) {
     var str = this;
     var a;
     var b;
@@ -187,10 +198,11 @@ String.prototype.replaceChars = function(character, replacement){
             str = a + b;
         }
     }
+
     return str;
 };
 
-window.onunload = function(){
+window.onunload = function() {
     delete window.cfg;
     delete window.cfg_bool;
 };
